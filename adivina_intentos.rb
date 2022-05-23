421ca60246e7fc2f85a0593ad64a27a88366ff2f
Oscar Nuñez <98293549+Oskarnuz@users.noreply.github.com>
2022-05-22 19:41:45 -0500

    Create program guess attemps

--- adivina_intentos.rb
+++ adivina_intentos.rb
@@ -6,12 +6,12 @@ while num_attempts < 3
     print "Adivina el número de 1 a 10 que estoy pensando: "
     guess = gets.chomp.to_i
   
-        if guess == number
+        if guess == number 
             puts "Felicitaciones, ese era!"
             
         else
             puts "Lo siento, intenta nuevamente!" 
             num_attempts += 1
         end
-      
+break  if guess == number      
 end
