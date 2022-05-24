4dffaf511da980b2b293de6ff0d51faeffecb10f
Oscar Nuñez <98293549+Oskarnuz@users.noreply.github.com>
2022-05-23 21:39:32 -0500

    Update the code

--- contrasenas.rb
+++ contrasenas.rb
@@ -1,6 +1,11 @@
 #Use def for to define a methode
 def contrasena(str)
-    str.gsub(/[aeio ]/, 'a' => 4 , 'e' => 3 , 'i' => 1 , 'o' => 0 , ' ' => '')
+    str = str.gsub("a", "4")
+    str = str.gsub("e", "3")
+    str = str.gsub("i", "1")
+    str = str.gsub("o", "0")
+    str = str.gsub(" ", "")
+    #str.gsub(/[aeio ]/, 'a' => 4 , 'e' => 3 , 'i' => 1 , 'o' => 0 , ' ' => '')
   end
   
   puts contrasena("hola") # "h0l4"
