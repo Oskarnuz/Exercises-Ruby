09de76ffcd2fddb0fc6ea8e444efc759886e113e
Oscar Nuñez <98293549+Oskarnuz@users.noreply.github.com>
2022-05-26 08:14:46 -0500

    I agree the pseudocode

--- adivina.rb
+++ adivina.rb
@@ -1,4 +1,11 @@
-# Adivina el número
+=begin 
+Adivina el número
+1. Generar un numero de forma aleatoria
+2. Pedir al usuario que adivine el numero
+3. Evaluar si el numero generado y el numero adivinado son iguales
+4. Si son iguales Imprimir "Felicitaciones, ese era!
+5. Si no son iguales imprimir "Lo siento, intenta nuevamente"
+=end
 
 print "Adivina el número que estoy pensando: "
 
