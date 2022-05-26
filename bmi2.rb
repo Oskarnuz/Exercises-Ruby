88204fde9b5bd52a6028a16aed957d36e9401930
Oscar Nuñez <98293549+Oskarnuz@users.noreply.github.com>
2022-05-26 09:30:04 -0500

    I include the pseudocode

--- bmi2.rb
+++ bmi2.rb
@@ -1,3 +1,16 @@
+=begin
+Programa par calcular el IMC
+1. Solicitar al usuario que ingrese el peso y guardarlo en una variable
+2. Solicitar al usario que ingrese l aaltura y guardarla en una variable
+3. Calcular el BMI dividiendo el peso entre la altura al cuadrado
+4. Imprimir el valor del IMC
+5. Evaluar el bmi con las siguientes condiciones e imprimir las observaciones segun la condicion:
+    a. "Bajo de peso" si el BMI < 18.5
+    b. "Normal" si está entre 18.5 y 24.9
+    c. "Sobrepeso" si está entre 25 y 29.9
+    d. "Obeso" si es igual o mayor a 30
+=end
+
 print "Ingresa tu peso: "
 weight = gets.chomp.to_i
 
