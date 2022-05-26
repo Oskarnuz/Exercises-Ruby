43b53e68eafb90ebf348b561ac1c39f6ca8986bc
Oscar Nuñez <98293549+Oskarnuz@users.noreply.github.com>
2022-05-26 09:23:28 -0500

    I include the pseudocode

--- adivina_intentos.rb
+++ adivina_intentos.rb
@@ -1,6 +1,15 @@
+=begin 
+Adivina el número
+1. Generar un numero de forma aleatoria (para el caso del ejercicio se dejara un numero fijo para la prueba)
+2. Pedir al usuario que adivine el numero
+3. Evaluar si el numero generado y el numero adivinado son iguales
+4. Evaluar cuantos intentos va (Se limitara a un numero determinado)
+5. Si son iguales Imprimir "Felicitaciones, ese era!
+6. Si no son iguales imprimir "Lo siento, intenta nuevamente"
+7. Despues de superar los intentos sin adivinar el programa se termina 
+=end
 
 number = 4
-
 num_attempts = 0
 while num_attempts < 3
     print "Adivina el número de 1 a 10 que estoy pensando: "
