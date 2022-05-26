af1350be5d3c0717ce721375a3796219e2c56f42
Oscar Nuñez <98293549+Oskarnuz@users.noreply.github.com>
2022-05-26 08:54:10 -0500

    I include the pseudocode

--- bmi.rb
+++ bmi.rb
@@ -1,3 +1,12 @@
+=begin
+Programa par acalcular el IMC
+1. Solicitar al usuario que ingrese el peso y guardarlo en una variable
+2. Solicitar al usario que ingrese l aaltura y guardarla en una variable
+3. Calcular el BMI dividiendo el peso entre la altura al cuadrado
+4. Imprimir el valor del IMC
+=end
+
+
 print "Ingresa tu peso: "
 weight = gets.chomp.to_i
 
